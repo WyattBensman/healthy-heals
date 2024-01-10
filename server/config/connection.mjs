38b84f4df7956b8fbbbd7bbdbccb755abb82b64c
WyattBensman <wyattbensman5@gmail.com>
2024-01-10 13:41:07 -0500

    Fixed MongoDB connection

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -1,7 +1,21 @@
 import mongoose from "mongoose";
 
-const db = mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/healthy-heals"
-);
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/healthy-heals";
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 export default db;
